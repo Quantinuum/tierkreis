0798d03bb2aa8d357ffc469455f28fc1c77e28ee
Matthew Burke <matthew.burke@quantinuum.com>
2025-12-08 13:22:40 +0000

    Fix linter errors

--- tierkreis_visualization/frontend/src/graph/updateGraph.ts
+++ tierkreis_visualization/frontend/src/graph/updateGraph.ts
@@ -1,7 +1,113 @@
 import { bottomUpLayout } from "./layoutGraph";
 import { Graph } from "./models";
-import { loc_peek } from "@/data/loc";
+import { loc_children, loc_peek } from "@/data/loc";
+import { PyEdge, PyNode } from "@/data/api_types";
 import { getContainingNodes } from "@/nodes/layout";
+
+const DELETE_TAG = "fordeletion";
+
+export const amalgamateGraphData = (
+  evalData: Record<string, { nodes: PyNode[]; edges: PyEdge[] }>,
+  openEvals: string[],
+  openLoops: string[],
+  openMaps: string[]
+): {
+  nodes: PyNode[];
+  edges: PyEdge[];
+} => {
+  const ns = [];
+  let es = [];
+
+  for (const loc in evalData) {
+    ns.push(...(evalData[loc]?.nodes ?? []));
+    es.push(...(evalData[loc]?.edges ?? []));
+  }
+
+  // Rewire inputs of open MAPs
+  for (const e of es) {
+    if (!openMaps.includes(e.to_node)) continue;
+
+    const newTargets = loc_children(
+      e.to_node,
+      ns.map((x) => x.id)
+    );
+    const newEdges = newTargets.map((x) => {
+      return { ...e, to_node: x };
+    });
+    e.to_node = DELETE_TAG;
+    es = [...es, ...newEdges];
+  }
+
+  // Rewire outputs of open MAPs
+  for (const e of es) {
+    if (!openMaps.includes(e.from_node)) continue;
+
+    const newSources = loc_children(
+      e.from_node,
+      ns.map((x) => x.id)
+    );
+    const newEdges = newSources.map((x) => {
+      return { ...e, from_node: x };
+    });
+    e.from_node = DELETE_TAG;
+    es = [...es, ...newEdges];
+  }
+
+  // Rewire inputs of open LOOPs
+  for (const e of es) {
+    if (!openLoops.includes(e.to_node)) continue;
+
+    const newTargets = loc_children(
+      e.to_node,
+      ns.map((x) => x.id)
+    );
+    const newEdges = newTargets.map((x) => {
+      return { ...e, to_node: x };
+    });
+    e.to_node = DELETE_TAG;
+    es = [...es, ...newEdges];
+  }
+
+  // Rewire outputs of open LOOPs
+  for (const e of es) {
+    if (!openLoops.includes(e.from_node)) continue;
+
+    const newSources = loc_children(
+      e.from_node,
+      ns.map((x) => x.id)
+    );
+    const newEdges = newSources.map((x) => {
+      return { ...e, from_node: x };
+    });
+    e.from_node = DELETE_TAG;
+    es = [...es, ...newEdges];
+  }
+
+  // Rewire inputs of open EVALs
+  for (const e of es) {
+    if (!openEvals.includes(e.to_node)) continue;
+    if (e.to_port === "body") continue;
+
+    const newTarget = evalData[e.to_node]?.nodes.find(
+      (x) => x.function_name === "input" && x.value === e.to_port
+    );
+    if (newTarget !== undefined) e.to_node = newTarget.id;
+  }
+
+  // Rewire outputs of open EVALs
+  for (const e of es) {
+    if (!openEvals.includes(e.from_node)) continue;
+
+    const newSource = evalData[e.from_node]?.nodes.find(
+      (x) => x.function_name === "output"
+    );
+    if (newSource !== undefined) e.from_node = newSource.id;
+  }
+
+  es = es.filter((x) => x.to_node !== DELETE_TAG && x.from_node !== DELETE_TAG);
+
+  return { nodes: ns, edges: es };
+};
 
 export const updateGraph = (graph: Graph, new_graph: Graph): Graph => {
   const nodesMap = new Map(graph.nodes.map((node) => [node.id, node]));
