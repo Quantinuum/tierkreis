2d7dae3c55ce8fc84606f462aea13d9facc5aa9d
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-13 09:17:56 +0000

    Improve names

--- tierkreis/tierkreis/pkg/github.py
+++ tierkreis/tierkreis/pkg/github.py
@@ -16,7 +16,7 @@ class GitHubDependency(TKRDependency):
     branch: str = "main"
 
     def install(self, worker_name: str, target_dir: Path):
-        cache_dir = WORKER_CACHE / self.account / self.repo
+        cache_dir = WORKER_CACHE / "github" / self.account / self.repo
         cache_dir.mkdir(exist_ok=True, parents=True)
 
         git_dir = cache_dir / ".git"
