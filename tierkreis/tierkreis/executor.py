9d98ad520551bc5eb365fbed6d68fbc6a2113a11
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-15 11:15:58 +0000

    Expose TaskExecutor

--- tierkreis/tierkreis/executor.py
+++ tierkreis/tierkreis/executor.py
@@ -2,6 +2,12 @@ from tierkreis.controller.executor.shell_executor import ShellExecutor
 from tierkreis.controller.executor.uv_executor import UvExecutor
 from tierkreis.controller.executor.multiple import MultipleExecutor
 from tierkreis.controller.executor.hpc.pjsub import PJSUBExecutor
+from tierkreis.controller.executor.task_executor import TaskExecutor
 
-
-__all__ = ["ShellExecutor", "UvExecutor", "MultipleExecutor", "PJSUBExecutor"]
+__all__ = [
+    "ShellExecutor",
+    "UvExecutor",
+    "MultipleExecutor",
+    "PJSUBExecutor",
+    "TaskExecutor",
+]
