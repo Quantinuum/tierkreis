556354698e189cae09d8f4056d1508dbadfb8367
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-15 12:48:46 +0000

    Get workflow_dir

--- tierkreis/tierkreis/controller/executor/task_executor.py
+++ tierkreis/tierkreis/controller/executor/task_executor.py
@@ -4,6 +4,7 @@ from fnmatch import filter
 
 from tierkreis.controller.data.location import WorkerCallArgs
 from tierkreis.controller.executor.protocol import ControllerExecutor
+from tierkreis.controller.storage.protocol import ControllerStorage
 from tierkreis.exceptions import TierkreisError
 
 
@@ -13,11 +14,14 @@ class TaskExecutor:
     The fully qualified task name is of the form <WORKER_NAME>.<TASK_NAME> .
     Glob syntax can be used to route multiple tasks to the same executor."""
 
-    def __init__(self, assignments: dict[str, ControllerExecutor]) -> None:
+    def __init__(
+        self, assignments: dict[str, ControllerExecutor], storage: ControllerStorage
+    ) -> None:
         self.assignments = assignments
+        self.workflow_dir = storage.workflow_dir
 
     def run(self, launcher_name: str, worker_call_args_path: Path) -> None:
-        with open(worker_call_args_path) as fh:
+        with open(self.workflow_dir.parent / worker_call_args_path) as fh:
             call_args = WorkerCallArgs(**json.load(fh))
 
         qualified_task = f"{launcher_name}.{call_args.function_name}"
