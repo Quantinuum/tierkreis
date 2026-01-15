89f2de293dabc62a6de143fc43fed82e63f7ba07
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-01-15 10:35:21 +0000

    Update failing test

--- tierkreis/tests/cli/test_run_workflow.py
+++ tierkreis/tests/cli/test_run_workflow.py
@@ -1,17 +1,17 @@
-import pytest
 import json
 from pathlib import Path
-from uuid import UUID
 from unittest import mock
+from uuid import UUID
 
+import pytest
 
-from tierkreis.controller.data.graph import GraphData
-from tierkreis.cli.run_workflow import run_workflow
 from tests.controller.sample_graphdata import simple_eval
+from tierkreis.cli.run_workflow import run_workflow
+from tierkreis.controller.data.graph import GraphData
 from tierkreis.controller.data.types import ptype_from_bytes
 
 
-@pytest.fixture()
+@pytest.fixture
 def graph() -> GraphData:
     return simple_eval()
 
@@ -19,7 +19,7 @@ def graph() -> GraphData:
 def test_run_workflow(graph: GraphData) -> None:
     inputs = {}
     run_workflow(inputs=inputs, graph=graph, run_id=31415)
-    with open(
+    with Path.open(
         Path.home()
         / ".tierkreis"
         / "checkpoints"
@@ -32,18 +32,24 @@ def test_run_workflow(graph: GraphData) -> None:
     assert c == 12
 
 
-def test_run_workflow_with_output(graph: GraphData, capfd) -> None:
+def test_run_workflow_with_output(graph: GraphData, capfd) -> None:  # noqa: ANN001
     inputs = {}
     run_workflow(inputs=inputs, graph=graph, run_id=31415, print_output=True)
     out, _ = capfd.readouterr()
     assert "simple_eval_output: b'12'\n" in out
 
 
-@mock.patch("uuid.uuid4", return_value=UUID(int=31415))
-def test_run_workflow_default_run_id(_, graph: GraphData) -> None:
+@pytest.fixture
+def _patch_uuid4() -> mock.Mock:
+    with mock.patch("uuid.uuid4", return_value=UUID(int=31415)) as m:
+        return m
+
+
+@pytest.mark.usefixtures("_patch_uuid4", "graph")
+def test_run_workflow_default_run_id(graph: GraphData) -> None:
     inputs = {}
     run_workflow(inputs=inputs, graph=graph)
-    with open(
+    with Path.open(
         Path.home()
         / ".tierkreis"
         / "checkpoints"
@@ -62,5 +68,5 @@ def test_run_workflow_uv_executor(graph: GraphData) -> None:
         graph=graph,
         run_id=31415,
         use_uv_worker=True,
-        registry_path=Path("."),
+        registry_path=Path(),
     )
