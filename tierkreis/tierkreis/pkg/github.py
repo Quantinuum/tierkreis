c766c8bf81caca624be53859faeb3deb3782285c
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-15 09:39:48 +0000

    Clean Git before checkout

--- tierkreis/tierkreis/pkg/github.py
+++ tierkreis/tierkreis/pkg/github.py
@@ -24,6 +24,7 @@ class GitHubDependency(TKRDependency):
         if not git_dir.exists():
             subprocess.run(["git", "clone", git_url, "."], cwd=cache_dir)
 
+        subprocess.run(["git", "clean", "-f"], cwd=cache_dir)
         subprocess.run(["git", "checkout", self.branch], cwd=cache_dir)
         subprocess.run(["git", "pull"], cwd=cache_dir)
 
