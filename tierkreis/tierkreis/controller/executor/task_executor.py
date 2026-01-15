20212fcd429773674648ccf19dd12f3751cb4ede
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-15 11:08:26 +0000

    Raise if no glob matches

--- tierkreis/tierkreis/controller/executor/task_executor.py
+++ tierkreis/tierkreis/controller/executor/task_executor.py
@@ -4,9 +4,15 @@ from fnmatch import filter
 
 from tierkreis.controller.data.location import WorkerCallArgs
 from tierkreis.controller.executor.protocol import ControllerExecutor
+from tierkreis.exceptions import TierkreisError
 
 
 class TaskExecutor:
+    """A Tierkreis executor that routes tasks to other executors based on the fully qualified task name.
+
+    The fully qualified task name is of the form <WORKER_NAME>.<TASK_NAME> .
+    Glob syntax can be used to route multiple tasks to the same executor."""
+
     def __init__(self, assignments: dict[str, ControllerExecutor]) -> None:
         self.assignments = assignments
 
@@ -20,3 +26,5 @@ class TaskExecutor:
             if matching:
                 executor.run(launcher_name, worker_call_args_path)
                 return
+
+        raise TierkreisError(f"No assigned executor for task {qualified_task}")
