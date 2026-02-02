07a3a00ab1a6c230503e2a88f461f4acde27f9da
Matthew Burke <matthew.burke@quantinuum.com>
2026-02-02 09:31:01 +0000

    Improve EntryNotFound exception

--- tierkreis/tierkreis/controller/storage/exceptions.py
+++ tierkreis/tierkreis/controller/storage/exceptions.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from tierkreis.exceptions import TierkreisError
 
 
@@ -7,3 +8,11 @@ class TierkreisStorageError(TierkreisError):
 
 class EntryNotFound(TierkreisStorageError):
     """Storage entry not found."""
+
+    path: Path
+
+    def __init__(self, path: Path) -> None:
+        self.path = path
+
+    def __str__(self) -> str:
+        return str(self.path)
