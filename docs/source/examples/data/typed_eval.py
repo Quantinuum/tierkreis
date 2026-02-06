9ea27add5fcea75fd53737bfa8d3ce8685f98d9a
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-02-06 14:59:22 +0000

    Update import in restart example

---
+++ docs/source/examples/data/typed_eval.py
@@ -0,0 +1,49 @@
+from typing import NamedTuple
+from tierkreis.builtins.stubs import iadd, itimes
+from tierkreis.controller.data.core import EmptyModel
+from tierkreis.builder import GraphBuilder
+from tierkreis.controller.data.models import TKR
+
+
+class DoublerInput(NamedTuple):
+    x: TKR[int]
+    intercept: TKR[int]
+
+
+class DoublerOutput(NamedTuple):
+    a: TKR[int]
+    value: TKR[int]
+
+
+def typed_doubler():
+    g = GraphBuilder(TKR[int], TKR[int])
+    out = g.task(itimes(a=g.const(2), b=g.inputs))
+    g.outputs(out)
+    return g
+
+
+def typed_doubler_plus_multi():
+    g = GraphBuilder(DoublerInput, DoublerOutput)
+    mul = g.task(itimes(a=g.inputs.x, b=g.const(2)))
+    out = g.task(iadd(a=mul, b=g.inputs.intercept))
+    g.outputs(DoublerOutput(a=g.inputs.x, value=out))
+    return g
+
+
+def typed_doubler_plus():
+    g = GraphBuilder(DoublerInput, TKR[int])
+    mul = g.task(itimes(a=g.inputs.x, b=g.const(2)))
+    out = g.task(iadd(a=mul, b=g.inputs.intercept))
+    g.outputs(out)
+    return g
+
+
+class TypedEvalOutputs(NamedTuple):
+    typed_eval_output: TKR[int]
+
+
+def typed_eval():
+    g = GraphBuilder(EmptyModel, TypedEvalOutputs)
+    e = g.eval(typed_doubler_plus(), DoublerInput(x=g.const(6), intercept=g.const(0)))
+    g.outputs(TypedEvalOutputs(typed_eval_output=e))
+    return g
