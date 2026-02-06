afa231355c0c90a893d693d1b33d7d67e08a5bd3
Matthew Burke <matthew.burke@quantinuum.com>
2026-02-06 14:30:25 +0000

    Add some comments and logging

--- tierkreis/tierkreis/pkg/__init__.py
+++ tierkreis/tierkreis/pkg/__init__.py
@@ -1,8 +1,15 @@
+"""Manage dependencies for a Tierkreis project in the standard directory layout.
+
+Usually the target directory is the `workers_external` directory of the project."""
+
+import logging
 from os import unlink
 from pathlib import Path
 from shutil import rmtree
 from tierkreis.consts import WORKER_CACHE
 from tierkreis.pkg.base import TKRDependency
+
+logger = logging.getLogger(__name__)
 
 
 def install_dependencies(deps: dict[str, TKRDependency], target_dir: Path):
@@ -12,8 +19,21 @@ def install_dependencies(deps: dict[str, TKRDependency], target_dir: Path):
 
 
 def remove_dependencies(deps: list[str], target_dir: Path):
+    """Remove the worker directories listed in `deps` from the `target_dir`.
+
+    Assumes that the worker directory is a symlink or a folder."""
+
     for dep in deps:
-        unlink(target_dir / dep)
+        worker_dir = target_dir / dep
+        if worker_dir.is_symlink():
+            unlink(worker_dir)
+        elif worker_dir.is_dir():
+            rmtree(worker_dir)
+        else:
+            logger.warning(
+                f"Expected {worker_dir} to be a symlink or directory."
+                " Taking no action."
+            )
 
 
 def clear_cache():
