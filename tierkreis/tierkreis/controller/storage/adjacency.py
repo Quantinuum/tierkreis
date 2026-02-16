37abedf7c0a1aa45c6218bd15939a8fa8d9f501f
Matthew Burke <matthew.burke@quantinuum.com>
2026-02-16 09:36:13 +0000

    Clean up unfinished_inputs logic

--- tierkreis/tierkreis/controller/storage/adjacency.py
+++ tierkreis/tierkreis/controller/storage/adjacency.py
@@ -11,11 +11,9 @@ logger = logging.getLogger(__name__)
 def unfinished_inputs(
     storage: ControllerStorage, loc: Loc, node: NodeDef
 ) -> list[ValueRef]:
-    raw_ins = in_edges(node)
-    if node.type == "ifelse":  # ifelse is lazy: only wait for pred before starting
-        _ = raw_ins.pop("if_true", None)
-        _ = raw_ins.pop("if_false", None)
-    ins = [x for x in raw_ins.values() if x[0] >= 0]  # inputs at -1 already finished
+    # ifelse is lazy: only wait for pred before starting
+    ins = [node.pred] if node.type == "ifelse" else in_edges(node).values()
+    ins = [x for x in ins if x[0] >= 0]  # inputs at -1 already finished
     return [x for x in ins if not storage.is_node_finished(loc.N(x[0]))]
 
 
