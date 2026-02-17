7dedd50e7829e33494880374ff2e7bb6b390ec1f
Matthew Burke <matthew.burke@quantinuum.com>
2026-02-17 08:48:01 +0000

    Inline graph to refer to NodeIndexes

--- tierkreis/tests/controller/test_restart.py
+++ tierkreis/tests/controller/test_restart.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from uuid import UUID
 
-
 from tierkreis import run_graph
 from tierkreis.builder import GraphBuilder
 from tierkreis.builtins.stubs import iadd
@@ -11,26 +10,28 @@ from tierkreis.executor import UvExecutor
 from tierkreis.storage import FileStorage, read_outputs
 
 
-def diamond_graph() -> GraphBuilder[TKR[int], TKR[int]]:
+def test_restart():
+    storage = FileStorage(UUID(int=300), "test_restart")
+    storage.clean_graph_files()
+    executor = UvExecutor(Path(__file__).parent.parent / "workers", storage.logs_path)
+
     g = GraphBuilder(TKR[int], TKR[int])
     plus_one = g.task(iadd(g.const(1), g.inputs))
     left = g.task(iadd(g.const(1), plus_one))
     right = g.task(iadd(g.const(1), plus_one))
     out = g.task(iadd(left, right))
     g.outputs(out)
-    return g
 
+    run_graph(storage, executor, g, {"value": 0})
 
-def test_restart():
-    storage = FileStorage(UUID(int=300), "test_restart")
-    storage.clean_graph_files()
-    executor = UvExecutor(Path(__file__).parent.parent / "workers", storage.logs_path)
-
-    run_graph(storage, executor, diamond_graph(), {"value": 0})
-
-    outputs = read_outputs(diamond_graph(), storage)
+    outputs = read_outputs(g, storage)
     assert outputs == 4
 
-    invalidated = storage.restart_task(Loc().N(2))
-    expected = [Loc().N(4), Loc().N(6), Loc().N(7), Loc().N(8)]
+    invalidated = storage.restart_task(Loc().N(plus_one.node_index))
+    expected = [
+        Loc().N(left.node_index),
+        Loc().N(right.node_index),
+        Loc().N(out.node_index),
+        Loc().N(g.get_data().output_idx()),
+    ]
     assert sorted(invalidated) == sorted(expected)
