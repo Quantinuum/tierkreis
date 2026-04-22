6f0f39f6ddbaf2df67a1e2a968d2aeb84c2d1e12
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-04-22 11:09:52 +0100

    Add docstrings

--- tierkreis_workers/guppy_worker/tkr_guppy_worker_impl/impl.py
+++ tierkreis_workers/guppy_worker/tkr_guppy_worker_impl/impl.py
@@ -1,19 +1,16 @@
 from typing import Annotated, Any
 
 from guppylang import comptime, guppy
-from guppylang.emulator import EmulatorResult, EmulatorBuilder
-
-from hugr.package import Package
-from hugr.qsystem.result import QsysShot, DataValue
-from pytket import Circuit
-from pytket.backends.backendresult import BackendResult
-
-from tierkreis import Worker
-from tierkreis.controller.data.core import Deserializer, Serializer
-
+from guppylang.emulator import EmulatorBuilder, EmulatorResult
 from guppylang.std.builtins import array, result
 from guppylang.std.quantum import cx, h, measure_array, qubit
+from hugr.package import Package
+from hugr.qsystem.result import DataValue, QsysShot
+from pytket import Circuit
+from pytket.backends.backendresult import BackendResult
+from tierkreis.controller.data.core import Deserializer, Serializer
 
+from tierkreis import Worker
 
 worker = Worker("guppy_worker")
 
@@ -41,6 +38,19 @@ def emulate(
     n_shots: int,
     seed: int | None = None,
 ) -> Result:
+    """Emulate a hugr on a local emulator.
+
+    :param hugr_package: The package to emulate.
+    :type hugr_package: Package
+    :param n_qubits: The number of qubits to emulate.
+    :type n_qubits: int
+    :param n_shots: The number of shots to take.
+    :type n_shots: int
+    :param seed: The seed for the random number generator, defaults to None
+    :type seed: int | None, optional
+    :return: The emulator result state.
+    :rtype: Result
+    """
     emulator = (
         EmulatorBuilder()
         .build(hugr_package, n_qubits)
@@ -53,16 +63,41 @@ def emulate(
 
 @worker.task()
 def from_circuit(circuit: Circuit, name: str, use_arrays: bool = True) -> Package:
+    """Build a hugr from a pytket circuit.
+
+    :param circuit: The circuit to build the hugr from.
+    :type circuit: Circuit
+    :param name: The name of the hugr.
+    :type name: str
+    :param use_arrays: Use qubit arrays, defaults to True
+    :type use_arrays: bool, optional
+    :return: The compiled hugr.
+    :rtype: Package
+    """
     return guppy.load_pytket(name, circuit, use_arrays=use_arrays).compile()
 
 
 @worker.task()
 def to_backend_result(results: Result) -> BackendResult:
+    """Transform a new stack result to a pytket result.
+
+    :param results: The original result.
+    :type results: Result
+    :return: The corresponding pytket result.
+    :rtype: BackendResult
+    """
     return results.to_pytket()
 
 
 @worker.task()
 def ghz(size: int) -> Package:
+    """Build a n-qubit GHZ quantum state as Hugr.
+
+    :param size: The size of the state.
+    :type size: int
+    :return: A Hugr representing the GHZ state.
+    :rtype: Package
+    """
     n = guppy.nat_var("n")
 
     @guppy
--- tierkreis_workers/nexus_worker/tkr_nexus_worker_impl/impl.py
+++ tierkreis_workers/nexus_worker/tkr_nexus_worker_impl/impl.py
@@ -120,6 +120,17 @@ def upload_hugr(
     project_name: str,
     name: str | None = None,
 ) -> HUGRRef:
+    """Upload a Hugr to nexus
+
+    :param hugr_package: The Hugr package to upload.
+    :type hugr_package: Package
+    :param project_name: The name of the nexus project to upload the Hugr to.
+    :type project_name: str
+    :param name: The name of the Hugr, defaults to a fallback name with timestamp.
+    :type name: str | None, optional
+    :return: The reference to the uploaded Hugr.
+    :rtype: HUGRRef
+    """
     if name is None:
         name = f"tkr HUGR Package from {datetime.now()}"
     my_project_ref = qnx.projects.get_or_create(name=project_name)
@@ -129,6 +140,17 @@ def upload_hugr(
 
 @worker.task()
 def cost(hugr_ref: HUGRRef | list[HUGRRef], n_shots: int, project_name: str) -> float:
+    """Estimate the cost of running the Hugrs.
+
+    :param hugr_ref: The reference(s) to the Hugr(s) to estimate the cost for.
+    :type hugr_ref: HUGRRef | list[HUGRRef]
+    :param n_shots: The number of shots to estimate the cost for.
+    :type n_shots: int
+    :param project_name: The name of the nexus project to estimate the cost in.
+    :type project_name: str
+    :return: The estimated cost.
+    :rtype: float
+    """
     my_project_ref = qnx.projects.get_or_create(name=project_name)
     qnx.context.set_active_project(my_project_ref)
     return qnx.hugr.cost(hugr_ref, n_shots)
