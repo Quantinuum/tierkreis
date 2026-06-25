07816943b57ac3943632d00ef6df35125f183dd0
John Children <32305209+johnchildren@users.noreply.github.com>
2026-06-25 16:29:44 +0100

    chore: Disallow optional default inputs in graphs (#523)
    Revert change to support not passing inputs to graphs as it lead to
    potentially surprising behaviour. This functionality will need to be
    reviewed again for the new runtime.

--- tierkreis/src/orchestrator.rs
+++ tierkreis/src/orchestrator.rs
@@ -230,7 +230,7 @@ impl Orchestrator {
                 let loc = parent_location.with_node(n);
                 match definition {
                     NodeDefinition::Input { name } => stream_action_result(
-                        self.build_input_action(graph_inputs.clone(), loc, name),
+                        Self::build_input_action(graph_inputs.clone(), loc, name),
                     ),
                     NodeDefinition::Const { value } => {
                         stream_action_result(self.build_const_action(loc, value.clone()))
@@ -526,23 +526,17 @@ impl Orchestrator {
         })
     }
 
-    #[instrument(skip(self, graph_inputs), fields(loc = %loc), err)]
+    #[instrument(skip(graph_inputs), fields(loc = %loc), err)]
     fn build_input_action(
-        &self,
         graph_inputs: Arc<HashMap<String, AssetSpec>>,
         loc: Location,
         name: &str,
     ) -> miette::Result<Action> {
-        let value = graph_inputs.get(name).cloned().unwrap_or_else(|| {
-            save_asset(
-                &self.asset_storage_registry,
-                &self.default_storage_name,
-                serde_json::to_vec(&serde_json::Value::Null).unwrap(),
-            )
-            .unwrap()
-        });
+        let value = graph_inputs
+            .get(name)
+            .ok_or_else(|| miette!("Input not found for port: {name}"))?;
         let mut outputs = HashMap::new();
-        outputs.insert(name.to_string(), value);
+        outputs.insert(name.to_string(), value.clone());
 
         Ok(Action {
             loc,
