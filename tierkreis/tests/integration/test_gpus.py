6702f9754d0f9ced0aef0d5d4a40e3795fc3c063
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-07-03 16:43:16 +0100

    Update Graphs

--- tierkreis/tests/integration/test_gpus.py
+++ tierkreis/tests/integration/test_gpus.py
@@ -14,13 +14,12 @@ from pytket_worker import backend_result_to_dict
 
 
 from tierkreis.builder import Graph
-from tierkreis.builtins.stubs import append, at
 from tierkreis.controller import run_graph
 from tierkreis.controller.data.models import TKR, OpaqueType
 from tierkreis.controller.data.types import Workflow
 from tierkreis.controller.executor.hpc.pbs import PBSExecutor
 from tierkreis.controller.executor.multiple import MultipleExecutor
-from tierkreis.controller.executor.uv_executor import UvExecutor
+from tierkreis.controller.executor.shell_executor import ShellExecutor
 from tierkreis.controller.storage.filestorage import ControllerFileStorage
 from tierkreis.controller.storage.protocol import ControllerStorage
 from tierkreis.graphs.nexus.submit_poll_hugr import (
@@ -82,7 +81,7 @@ def build_ghz_state() -> None:
 class MyInputs(NamedTuple):
     shots: TKR[int]
     n_qubits: TKR[int]
-    package: TKR[Package]
+    package: TKR[list[Package]]
     project_name: TKR[str]
     job_name: TKR[str]
     backend_config: TKR[OpaqueType["qnx.BackendConfig"]]
@@ -96,22 +95,18 @@ class MyOutputs(NamedTuple):
 def guppy_nexus_graph() -> Workflow[MyInputs, MyOutputs]:
     graph = Graph(MyInputs, MyOutputs)
     gpus = graph.task(get_devices())
-    empty_list = graph.const([])
-    shots = graph.task(append(empty_list, graph.inputs.shots))
-    hugrs = graph.task(append(empty_list, graph.inputs.package))  # type: ignore
     simulate = graph.eval(
         nexus_submit_and_poll_hugr(),
         JobInputs(
             project_name=graph.inputs.project_name,
             job_name=graph.inputs.job_name,
-            n_shots=shots,
+            n_shots=graph.inputs.shots,
             backend_config=graph.inputs.backend_config,  # type: ignore
-            hugrs=hugrs,  # type: ignore
+            hugrs=graph.inputs.package,  # type: ignore
         ),
     )
-    out = graph.task(at(simulate, graph.const(0)))
-    final = graph.task(backend_result_to_dict(out))
-    return graph.finish_with_outputs(MyOutputs(results=final, devices=gpus))
+    final = graph.task(backend_result_to_dict(simulate))
+    return graph.finish_with_outputs(MyOutputs(results=final, devices=gpus))  # type: ignore
 
 
 def test_guppy_nexus_integration() -> None:
@@ -122,6 +117,7 @@ def test_guppy_nexus_integration() -> None:
         emulator_config=qnx.models.HeliosEmulatorConfig(
             n_qubits=3, simulator=qnx.models.StatevectorSimulator()
         ),
+        max_cost=100,
     )
     inputs = {
         "shots": 10,
@@ -135,11 +131,11 @@ def test_guppy_nexus_integration() -> None:
         UUID(int=1), name="test_integration", do_cleanup=True
     )
     pbs_executor = pbs(storage)
-    test_workers_path = Path(__file__).parent.parent / "workers"
-    default = UvExecutor(test_workers_path, storage.logs_path)
+    default = ShellExecutor(None, storage.workflow_dir)
     executor = MultipleExecutor(default, {"pbs": pbs_executor}, {"gpu_worker": "pbs"})
     storage.clean_graph_files()
-    run_graph(storage, executor, wf, inputs, polling_interval_seconds=5)
+    run_graph(storage, executor, wf, inputs, polling_interval_seconds=1)
+    # storage = debug_graph(wf, inputs)
     out = read_outputs(wf, storage)
     print(out)
     assert out is not None
