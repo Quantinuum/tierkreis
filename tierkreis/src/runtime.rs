fdfa57f7a7572e245d5fce7764b7069a1c76a3cf
John Children <john.children@quantinuum.com>
2026-07-03 13:50:39 +0100

    chore: Disable auto-initialize
    We aren't really using for anything anyway

--- tierkreis/src/runtime.rs
+++ tierkreis/src/runtime.rs
@@ -3,19 +3,12 @@ The runtime module defines the entrypoint to running Workflows.
 */
 use std::{
     collections::HashMap,
-    ffi::{CStr, CString},
-    fs::File,
     hash::BuildHasher,
-    io::Read,
     path::Path,
     sync::{Arc, RwLock},
 };
 
-use miette::{
-    Error, IntoDiagnostic, LabeledSpan, MietteDiagnostic, NamedSource, SourceOffset, SourceSpan,
-    miette,
-};
-use pyo3::{exceptions::PySyntaxError, prelude::*};
+use miette::{IntoDiagnostic, miette};
 use uuid::Uuid;
 
 use crate::{
@@ -24,29 +17,12 @@ use crate::{
         load_assets, save_assets,
     },
     executor::{Executor, InMemoryExecutor, SubprocessExecutor},
-    graph::{LegacyWorkflowGraph, WorkflowGraph},
+    graph::WorkflowGraph,
     location::Location,
     orchestrator::{OrchestrationContext, Orchestrator},
     state::{InMemoryRuntimeState, RuntimeState, SqliteRuntimeState, WorkflowRunState},
     updater::Updater,
 };
-
-/// Utility macro is make nicer diagnostics and return early when handling python exceptions.
-macro_rules! getattr_or_early_return {
-    ($obj:ident, $attr:expr) => {{
-        let attr_any = match $obj.getattr($attr).into_diagnostic() {
-            Ok(attr) => attr,
-            Err(err) => return err,
-        };
-
-        let attr = match attr_any.extract().into_diagnostic() {
-            Ok(attr) => attr,
-            Err(err) => return err,
-        };
-
-        attr
-    }};
-}
 
 struct Runtime<RS: RuntimeState> {
     orchestrator: Orchestrator,
@@ -311,101 +287,4 @@ pub(crate) async fn run_workflow_in_memory<S: BuildHasher>(
     let outputs = runtime.outputs(run_id, attempt).await?;
 
     Ok(outputs)
-}
-
-/// Run a Workflow in memory from a filepath to a python file containing a builder.
-///
-/// # Errors
-///
-/// Will return Err if various I/O issues occur, if the runtime is unusable or
-/// if the provided filepath includes invalid python.
-pub fn run(path: &Path) -> miette::Result<()> {
-    Python::attach(|py| {
-        let path = path.canonicalize().into_diagnostic()?;
-
-        let mut file = File::open(&path).into_diagnostic()?;
-        let mut code_buf = String::new();
-        file.read_to_string(&mut code_buf).into_diagnostic()?;
-        code_buf.push('\x00');
-
-        let code = CStr::from_bytes_until_nul(code_buf.as_bytes()).into_diagnostic()?;
-
-        let source_file_name = path.file_name().ok_or_else(|| miette!("no file name"))?;
-        let source_file_name_str = source_file_name
-            .to_str()
-            .ok_or_else(|| miette!("failed to convert to cstring"))?;
-        let source_file_name_cstring = CString::new(source_file_name_str).into_diagnostic()?;
-
-        let module = path.file_stem().ok_or_else(|| miette!("no file stem"))?;
-        let module_name_cstring = CString::new(
-            module
-                .to_str()
-                .ok_or_else(|| miette!("failed to convert to cstring"))?,
-        )
-        .into_diagnostic()?;
-
-        let module = PyModule::from_code(py, code, &source_file_name_cstring, &module_name_cstring)
-            .map_err(|err: PyErr| {
-                if err.is_instance_of::<PySyntaxError>(py) {
-                    let err_value = err.value(py);
-                    let message: String = getattr_or_early_return!(err_value, "msg");
-                    let lineno: usize = getattr_or_early_return!(err_value, "lineno");
-                    let offset: usize = getattr_or_early_return!(err_value, "offset");
-                    let end_offset: usize = getattr_or_early_return!(err_value, "end_offset");
-                    let error_filename: String = getattr_or_early_return!(err_value, "filename");
-
-                    let labels = vec![LabeledSpan::new_primary_with_span(
-                        Some(message.clone()),
-                        SourceSpan::new(
-                            SourceOffset::from_location(&code_buf, lineno, offset),
-                            end_offset - offset,
-                        ),
-                    )];
-                    let diagnostic = MietteDiagnostic::new(message).and_labels(labels).with_help(
-                        "Tierkreis requires a valid python module to construct a Workflow",
-                    );
-
-                    let source_code =
-                        NamedSource::new(error_filename, code_buf.clone()).with_language("Python");
-
-                    return Error::new(diagnostic).with_source_code(source_code);
-                }
-
-                miette!("Failed to load python module: {}", err.to_string())
-            })?;
-
-        let legacy_workflow_attr = module
-            .getattr("workflow")
-            .into_diagnostic()
-            .map_err(|err| {
-                let diagnostic = MietteDiagnostic::new("No 'workflow' attribute found in module")
-                    .with_help("Tierkreis requires an attribute called 'workflow'");
-                let rich_error = Error::new(diagnostic).with_source_code(
-                    NamedSource::new(source_file_name_str, code_buf.clone())
-                        .with_language("Python"),
-                );
-                rich_error.wrap_err(err)
-            })?;
-
-        let workflow_dump: String = legacy_workflow_attr
-            .call_method0("model_dump_json")
-            .into_diagnostic()?
-            .to_string();
-
-        let legacy_workflow: LegacyWorkflowGraph =
-            serde_json::from_str(&workflow_dump).into_diagnostic()?;
-        let workflow_graph = legacy_workflow.to_workflow_graph()?;
-
-        // TODO: We cannot yet provide inputs to the workflow with this function.
-        let outputs = run_workflow_in_memory(workflow_graph, HashMap::new())?;
-
-        let outputs: HashMap<String, serde_json::Value> = outputs
-            .into_iter()
-            .map(|(k, v)| Ok((k.clone(), serde_json::from_slice(&v).into_diagnostic()?)))
-            .collect::<miette::Result<_>>()?;
-
-        println!("{outputs:?}");
-
-        Ok(())
-    })
 }
