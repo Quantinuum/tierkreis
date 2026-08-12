894da11db1acd1ecbad5b2d05f71bda6d5da5d65
Philipp Seitz <philipp.seitz@quantinuum.com>
2026-08-12 14:34:03 +0100

    Update node status

--- tierkreis/src/event.rs
+++ tierkreis/src/event.rs
@@ -172,6 +172,9 @@ pub enum NodeStatus {
         /// A longer detailed context about the error.
         detail: Option<String>,
     },
+
+    /// The state of the node is unknown.
+    Unknown,
 }
 
 /// [`EventSender`] is an multi-producer single-consumer producer for [`Event`] messages.
